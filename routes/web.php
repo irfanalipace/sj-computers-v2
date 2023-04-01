d093715416fcaf40c9d1ccff51cf8161f23572e2
haroonmukhtar <haroonmukhtar4@gmail.com>
2023-04-01 09:53:35 +0500

    integrated react in laravel

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use App\Http\Controllers\Auth\RegisterController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
 
 Auth::routes();
---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+    return (
+        <div className="App">
+            <div className="row">
+                <div className="col-xl-2 col-lg-4 col-md-6 col-12">
+                    First coloumn
+                </div>
+                <div className="col-xl-2 col-lg-4 col-md-6 col-12">
+                    Second coloumn
+                </div>
+            </div>
+            <h1>Hello Index</h1>
+
+            <p>What a REAVELation</p>
+        </div>
+    );
+}
+
+export default App;
