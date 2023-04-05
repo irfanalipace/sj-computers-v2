d52f66ca26fbda06c4bd89ecc8df44633bfcd3b3
Irfan Ali <irfan.ali@99technologies.co>
2023-04-05 07:34:16 +0100

    m

--- webpack.config.js
+++ webpack.config.js
@@ -21,26 +21,18 @@ module.exports = {
                 test: /.scss$/,
                 use: ["style-loader", "css-loader", "sass-loader"],
             },
-            {
-                test: /\.(png|jpe?g|gif)$/i,
-                use: [
-                  {
-                    loader: 'file-loader',
-                    options: {
-                      name: '[name].[ext]',
-                      outputPath: 'images/',
-                    },
-                  },
-                  {
-                    loader: 'url-loader',
-                    options: {
-                      limit: 8192, // Convert images < 8kb to base64 strings
-                      name: '[name].[ext]',
-                      outputPath: 'images/',
-                    },
-                  },
-                ],
-              },
+           {
+                test: /\.(png|jpg)$/i,
+                type: 'asset',
+                parser: {
+                    dataUrlCondition: {
+                        maxSize: 10 * 1024 // Inline images under 10KB
+                    }
+                },
+                generator: {
+                    filename: 'images/[name]-[hash][ext]'
+                }
+            }
         ],
     },
 
