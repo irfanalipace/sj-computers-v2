19f10437c090c5d450b7dd64ea2ac1d842117c4c
Mahnoor Aftab <mahnoor@99technologies.co>
2023-04-10 16:05:27 +0500

    Fixed api.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,8 +14,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            RoleSeeder::class,
+            RolesTableSeeder::class,
             UserSeeder::class,
+//            UsersTableSeeder::class,
             CountryStateSeeder::class
         ]);
     }
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\RegisterController;
+use TCG\Voyager\Facades\Voyager;
 
 /*
 |--------------------------------------------------------------------------
