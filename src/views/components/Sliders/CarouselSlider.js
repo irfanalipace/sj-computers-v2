c20ea4a6ee96e0d58fb6aca87927c78fb103bc62
haroon.mukhtar <haroon.mukhtar@99technologies.co>
2023-04-11 13:24:29 +0500

    fixes

--- src/views/components/Sliders/CarouselSlider.js
+++ src/views/components/Sliders/CarouselSlider.js
@@ -1,82 +1,76 @@
-import React from 'react';
-import rect1 from '@images/product/CarsulSlider/rectangle1.png'
-import rect2 from '@images/product/CarsulSlider/rectangle2.png'
-import rect3 from '@images/product/CarsulSlider/rectangle3.png'
-import rect4 from '@images/product/CarsulSlider/rectangle4.png'
-import rect5 from '@images/product/CarsulSlider/rectangle5.png'
-import rect6 from '@images/product/CarsulSlider/rectangle6.png'
-import rect7 from '@images/product/CarsulSlider/rectangle7.png'
-import rect8 from '@images/product/CarsulSlider/rectangle8.png'
-import Carousel from 'react-bootstrap/Carousel';
-import './CarouselSlider.css'
-import { Link } from 'react-router-dom';
+import React from "react";
+import rect1 from "@images/product/CarsulSlider/rectangle1.png";
+import rect2 from "@images/product/CarsulSlider/rectangle2.png";
+import rect3 from "@images/product/CarsulSlider/rectangle3.png";
+import rect4 from "@images/product/CarsulSlider/rectangle4.png";
+import rect5 from "@images/product/CarsulSlider/rectangle5.png";
+import rect6 from "@images/product/CarsulSlider/rectangle6.png";
+import rect7 from "@images/product/CarsulSlider/rectangle7.png";
+import rect8 from "@images/product/CarsulSlider/rectangle8.png";
+import Carousel from "react-bootstrap/Carousel";
+import "./CarouselSlider.css";
+import { Link } from "react-router-dom";
 const CarouselSlider = () => {
-   
-        return (
-            <Carousel>
+    return (
+        <Carousel>
             <Carousel.Item>
-                <div className='row'>
-                    <div className='col-md-12'>
-                    <div className='product-section-add'>
-                        <div style={{textAlign:'left'}}>
-                        Best Selling Products {' '}<Link className='text-decoration-none'>see more</Link>
+                <div className="row">
+                    <div className="col-md-12">
+                        <div className="product-section-add">
+                            <div style={{ textAlign: "left" }}>
+                                Best Selling Products{" "}
+                                <Link className="text-decoration-none">
+                                    see more
+                                </Link>
+                            </div>
+
+                            <img src={rect1} className="img-slider-cursule" />
+                            <img src={rect2} className="img-slider-cursule" />
+                            <img src={rect3} className="img-slider-cursule" />
+                            <img src={rect4} className="img-slider-cursule" />
+                            <img src={rect5} className="img-slider-cursule" />
+                            <img src={rect6} className="img-slider-cursule" />
+                            <img src={rect7} className="img-slider-cursule" />
+                            <img src={rect8} className="img-slider-cursule" />
                         </div>
-           
-            
-              <img src={rect1} className='img-slider-cursule'/>
-              <img src={rect2}  className='img-slider-cursule'/>
-              <img src={rect3}  className='img-slider-cursule'/>
-              <img src={rect4}  className='img-slider-cursule'/>
-              <img src={rect5}  className='img-slider-cursule'/>
-                <img src={rect6}  className='img-slider-cursule'/>
-                <img src={rect7}  className='img-slider-cursule'/>
-                <img src={rect8}  className='img-slider-cursule' />
+                    </div>
                 </div>
-                </div>
-                
-                </div>
-              <Carousel.Caption>
-                {/* <h3>First slide label</h3>
+                <Carousel.Caption>
+                    {/* <h3>First slide label</h3>
                 <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p> */}
-              </Carousel.Caption>
+                </Carousel.Caption>
             </Carousel.Item>
-            
+
             <Carousel.Item>
-               
-                    <div className='col-md-12'>
-                    <div className='product-section-add'>
-                    <div style={{textAlign:'left'}}>
-                        Best Selling Products {' '}<Link className='text-decoration-none'>see more</Link>
+                <div className="col-md-12">
+                    <div className="product-section-add">
+                        <div style={{ textAlign: "left" }}>
+                            Best Selling Products{" "}
+                            <Link className="text-decoration-none">
+                                see more
+                            </Link>
                         </div>
-           
-                  
-              <img src={rect1} className='img-slider-cursule'/>
-              <img src={rect2}  className='img-slider-cursule'/>
-              <img src={rect3}  className='img-slider-cursule'/>
-              <img src={rect4}  className='img-slider-cursule'/>
-              <img src={rect5}  className='img-slider-cursule'/>
-                <img src={rect6}  className='img-slider-cursule'/>
-                <img src={rect7}  className='img-slider-cursule'/>
-                <img src={rect8}  className='img-slider-cursule' />
+
+                        <img src={rect1} className="img-slider-cursule" />
+                        <img src={rect2} className="img-slider-cursule" />
+                        <img src={rect3} className="img-slider-cursule" />
+                        <img src={rect4} className="img-slider-cursule" />
+                        <img src={rect5} className="img-slider-cursule" />
+                        <img src={rect6} className="img-slider-cursule" />
+                        <img src={rect7} className="img-slider-cursule" />
+                        <img src={rect8} className="img-slider-cursule" />
+                    </div>
                 </div>
-                </div>
-                
-                
-              <Carousel.Caption>
-                {/* <h3>First slide label</h3>
+
+                <Carousel.Caption>
+                    {/* <h3>First slide label</h3>
                 <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p> */}
-              </Carousel.Caption>
-              
+                </Carousel.Caption>
             </Carousel.Item>
-          
-              
-           
-
-          </Carousel>
-        );
-    }
-export default CarouselSlider
-
+        </Carousel>
+    );
+};
+export default CarouselSlider;
 
 // Don't forget to include the css in your page
 
--- src/views/pages/Home/Home.js
+++ src/views/pages/Home/Home.js
@@ -3,7 +3,7 @@ import Header from "@components/header/Header";
 import Footer from "@components/footer/footer";
 import BannerSlider from "@components/Sliders/BannerSlider";
 import { ProductType } from "@components/homeproduct/ProductType";
-import CarouselSlider from "@components/slider/CarouselSlider";
+import CarouselSlider from "@components/Sliders/CarouselSlider";
 import ProductThreeItem from "@components/homeproduct/product3category/ProductThreeItem";
 const Home = () => {
     return (
