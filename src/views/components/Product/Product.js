f3212a230de31efa226ce1e9175f5ad51a3849e2
haroonmukhtar <haroonmukhtar4@gmail.com>
2023-04-12 08:55:03 +0500

    m

--- src/views/components/Product/Product.js
+++
@@ -1,6 +0,0 @@
-import { useParams } from "react-router-dom";
-
-export default function Product() {
-    let { productId } = useParams();
-    return <div>Product {productId}</div>;
-}
--- src/views/pages/Product/Product.js
+++
@@ -1,7 +0,0 @@
-Product;
-import { useParams } from "react-router-dom";
-
-export default function Product() {
-    let { productId } = useParams();
-    return <div>Product {productId}</div>;
-}
