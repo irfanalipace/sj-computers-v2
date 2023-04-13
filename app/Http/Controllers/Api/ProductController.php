0ee018e82f569f01429c1d8b37a3c6c6cac200db
Mudassar Irshad <mduassarprogrammer@gmail.com>
2023-04-13 13:14:46 +0500

    set product

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -3,14 +3,14 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Http\Resources\Product;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends BaseController
 {
     public function getList(request $request){
         $data= Product::all();
-        $this->sendResponse($data);
+        return $this->sendResponse($data);
     }
 
 }
