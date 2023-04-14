8a43a5de9b439be5203a24f7f69e84b7db6ee9df
Mudassar Irshad <mduassarprogrammer@gmail.com>
2023-04-14 11:41:53 +0500

    initial amazon work

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -3,14 +3,14 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Models\Product;
+use App\Http\Resources\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends BaseController
 {
     public function getList(request $request){
         $data= Product::all();
-        return $this->sendResponse($data);
+        $this->sendResponse($data);
     }
 
 }
