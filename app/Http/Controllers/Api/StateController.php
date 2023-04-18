92e3a53d77622df0e92265416227d548ed59d4e6
Mudassar Irshad <mudassar.irshad@99technologies.co>
2023-04-18 13:29:43 +0500

    add states

--- app/Http/Controllers/Api/StateController.php
+++ app/Http/Controllers/Api/StateController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Models\State;
+use App\Models\UserState;
 use Illuminate\Http\Request;
 
 class StateController extends BaseController
@@ -10,5 +11,19 @@ class StateController extends BaseController
     public function getList(request $request){
         $data= State::all();
         return $this->sendResponse($data);
+    }
+
+    public function updateState(Request  $request){
+
+        UserState::updateOrCreate(['user_id' => auth()->user()->id],
+            [
+                'zip_code'=> $request->zip_code,
+                'state_id' => $request->state_id
+
+            ]
+        );
+
+        return $this->sendResponse();
+
     }
 }
