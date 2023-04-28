0f8d3f5b552b0a05547633cb868f31387537d53a
haroon.mukhtar <haroon.mukhtar@99technologies.co>
2023-04-28 18:53:46 +0500

    fix

--- src/core/api/cart.js
+++ src/core/api/cart.js
@@ -1,11 +1,8 @@
 import ApiService from "@services/apiService";
 
-export function addToCartApi({ id, quantity }) {
+export function addToCartApi(data) {
     return new Promise((resolve, reject) => {
-        ApiService.post(`/add-to-cart`, {
-            product_id: id,
-            qty: quantity,
-        })
+        ApiService.post(`/add-to-cart`, data)
             .then((response) => {
                 console.log(
                     "file: states.js | addToCartApi| response",
