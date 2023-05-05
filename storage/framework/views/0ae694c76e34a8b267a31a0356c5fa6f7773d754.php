241a61f75abda60bf0b4d3f72ebf644037e47cc0
mudassar <mudassarprogrammer@gmail.com>
2023-05-05 14:53:27 +0500

    clear

--- storage/framework/views/0ae694c76e34a8b267a31a0356c5fa6f7773d754.php
+++
@@ -1,3 +0,0 @@
-<?php echo e($slot); ?>
-
-<?php /**PATH C:\Users\Haroon.9985755\OneDrive - Higher Education Commission\Desktop\sj-computers\vendor\laravel\framework\src\Illuminate\Mail/resources/views/text/footer.blade.php ENDPATH**/ ?>
