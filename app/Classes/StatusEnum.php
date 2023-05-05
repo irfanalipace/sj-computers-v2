3f405de8367b7e86feb690484a23dd6369b9a887
mudassar <mudassarprogrammer@gmail.com>
2023-05-05 18:45:20 +0500

    set code of transaction

--- app/Classes/StatusEnum.php
+++ app/Classes/StatusEnum.php
@@ -8,11 +8,13 @@ class StatusEnum{
     public const PAYPALSUCCESSWITHWARNING = "SUCCESSWITHWARNING";
     public const SHIPPINGADDRESSCREATED = "Shipping address created successfully";
     public const SHIPPINGADDRESSUPDATED = "Shipping address updated successfully";
-    
+
     public const PAYMENTTYPEPAYPAL = "PAYPAL";
     public const PAYMENTTYPESQUARE = "SQUARE";
     public const COMPLETE = "COMPLETE";
 
     public const PAYMENTMESSAGE = "Payment Successfully completed";
+
+    public const DUMMY = "dummy";
 
 }
