2a9c925bfc986b40a451fe1c7d90921ac3ddf665
haris naseer satti <haris.naseer@99technologies.co>
2023-05-09 17:36:38 +0500

    fixes

--- app/Jobs/SendContactMail.php
+++ app/Jobs/SendContactMail.php
@@ -40,7 +40,7 @@ class SendContactMail implements ShouldQueue
         Log::info('send mail to contact us customer');
         $details = $this->data;
         Log::info($details['subject_name']);
-        Mail::send('email.contact-us',['data'=> $details], function ($m) use ($details) {
+        Mail::send('emails.contact-us',['data'=> $details], function ($m) use ($details) {
             $m->from(env('MAIL_FROM_ADDRESS'), config('app.name', 'APP Name'));
             $m->to($details['email'])->subject($details['subject_name']);
         });
