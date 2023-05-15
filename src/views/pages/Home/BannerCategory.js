b3b7684681290915d095e98c99cd61852cccdedf
haroon.mukhtar <haroon.mukhtar@99technologies.co>
2023-05-15 14:30:43 +0500

    fixes

--- src/views/pages/Home/BannerCategory.js
+++ src/views/pages/Home/BannerCategory.js
@@ -4,33 +4,82 @@ import BannerSlider from "@components/Sliders/BannerSlider";
 
 const BannerCategory = () => {
     const bannerSection = useRef(null);
+    const categorySection = useRef(null);
+    const productType = useRef(null);
+    const bannerInner = useRef(null);
 
-    const [height, setHeight] = useState("490px");
+    const [height, setHeight] = useState();
+    const [top, setTop] = useState("0");
+    const [categoryStyles, setCategoryStyles] = useState(null);
+    const [bannerHeight, setBannerHeight] = useState("0px");
 
     const changeHeight = () => {
-        setHeight(bannerSection.current.scrollHeight + "px");
+        setHeight(
+            bannerSection.current.scrollHeight / 2 +
+                categorySection.current.scrollHeight +
+                "px"
+        );
     };
 
     useEffect(() => {
-        // setHeight(bannerSection.current.scrollHeight + "px");
+        let screenWidth = window.innerWidth;
+        if (screenWidth > 768) {
+            setHeight(
+                bannerSection.current.scrollHeight +
+                    categorySection.current.scrollHeight -
+                    200 +
+                    "px"
+            );
+            setCategoryStyles({
+                height: productType.current.scrollHeight + "px",
+                top: "50%",
+            });
+        } else {
+            setHeight(
+                bannerSection.current.scrollHeight +
+                    categorySection.current.scrollHeight -
+                    70 +
+                    "px"
+            );
+            setCategoryStyles({
+                height: productType.current.scrollHeight + "px",
+                bottom: "0",
+                top: "unset",
+            });
+        }
+
+        setTop(bannerSection.current.scrollHeight / 1.5 + "px");
+        // setCategoryHeight(productType.current.scrollHeight + "px");
+        setBannerHeight(bannerInner.current.scrollHeight + "px");
         window.addEventListener("resize", () => {
             // changeHeight();
         });
-    }, [bannerSection.current]);
+    }, [bannerSection.current, categorySection.current, bannerInner.current]);
 
     return (
         <div
             className="banner-category-section"
             style={{
-                minHeight: height,
+                height,
             }}
-            ref={bannerSection}
         >
-            <div className="banner-wrapper">
-                <BannerSlider />
+            <div
+                className="banner-wrapper"
+                ref={bannerSection}
+                // style={{ height: bannerHeight }}
+            >
+                <div ref={bannerInner} className="banner-inner">
+                    <BannerSlider />
+                </div>
             </div>
-            <div className="catergory-grid-wrapper">
-                <ProductType />
+            <div
+                className="catergory-grid-wrapper"
+                ref={categorySection}
+                style={categoryStyles}
+            >
+                <div ref={productType}>
+                    <ProductType />
+                </div>
             </div>
         </div>
     );
