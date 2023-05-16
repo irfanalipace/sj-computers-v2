5d46e52d138ca129f9f2eff1ada1ea83b4e0acf6
mudassar <mudassarprogrammer@gmail.com>
2023-05-16 13:07:12 +0500

    set spelling

--- resources/views/emails/otp.blade.php
+++ resources/views/emails/otp.blade.php
@@ -8,7 +8,7 @@
     </tr>
     <tr>
         <td style="padding-bottom: 30px; padding-left: 20px; padding-right: 20px;" align="center" valign="top" class="subTitle">
-            <h4 class="text" style="color:#999;font-family:Poppins,Helvetica,Arial,sans-serif;font-size:16px;font-weight:500;font-style:normal;letter-spacing:normal;line-height:24px;text-transform:none;text-align:center;padding:0;margin:0">Verify Your Otp</h4>
+            <h4 class="text" style="color:#999;font-family:Poppins,Helvetica,Arial,sans-serif;font-size:16px;font-weight:500;font-style:normal;letter-spacing:normal;line-height:24px;text-transform:none;text-align:center;padding:0;margin:0">Verify Your OTP</h4>
         </td>
     </tr>
     <tr>
