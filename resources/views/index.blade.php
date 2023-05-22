da67d862af1d01e0d6c876b08bf5936199d53a3a
haroon.mukhtar <haroon.mukhtar@99technologies.co>
2023-05-22 17:42:42 +0500

    frontend fixes

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,6 +7,7 @@
 
     <meta name=“viewport” content=“width=device-width, initial-scale=1, shrink-to-fit=no”>
     <link rel="icon" href={{ asset('favicon.png') }} />
+    <!-- <link href={{ asset('/css/bootstrap.css') }}> -->
     <title>SJ-Computers</title>
 
 </head>
--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 
-import { BrowserRouter , useLocation} from "react-router-dom";
+import { BrowserRouter, useLocation } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { ToastContainer } from "react-toastify";
 
@@ -8,12 +8,15 @@ import { getToken } from "@services/jwtService";
 import { Router } from "@src/Routes";
 import { alreadyLoggedIn } from "@store/auth/authThunks";
 import { useInitDataFetching } from "@hooks/useInitDataFetching";
+
 import Header from "@components/Header/Header";
 import Footer from "@components/Footer/Footer";
 
 import "react-toastify/dist/ReactToastify.css";
 import "@fortawesome/fontawesome-free/css/all.css";
 import "./App.css";
+// import "./assets/bootstrap/bootstrap.css";
+
 import ScrollToTop from "./ScrollToTop";
 
 function App() {
@@ -57,8 +60,7 @@ function App() {
                     <Router />
                 </div>
 
-                 <Footer />
-
+                <Footer />
             </BrowserRouter>
         </div>
     );
