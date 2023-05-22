bab30feda590e1c10d8f314c1729032236af07a6
Irfan Ali <irfan.ali@99technologies.co>
2023-05-22 07:32:57 +0100

    22/05/2023

--- src/views/components/Auth/Header.js
+++ src/views/components/Auth/Header.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 import footerlogo from "@images/header-logo-black.png";
 // import footerlogo from path.resolve(__dirname, "src/assets/images/header-logo.png");
 //import footerlogo from path.join(__dirname, 'app', '@images/header-logo.png');
@@ -8,7 +8,10 @@ const Header = () => {
         <div className="container">
             <div className="row">
                 <div className="col-12">
-                    <img src={footerlogo} alt="" style={{ height: "58px" }} />
+                   <Link to='/'>
+                   <img src={footerlogo} alt="" style={{ height: "58px" }} />
+                   
+                   </Link>
                     {/* <p className='img-logo'>S.J. Computers</p>
                     <p className='img-logo-head'>your tech store !</p> */}
                 </div>
