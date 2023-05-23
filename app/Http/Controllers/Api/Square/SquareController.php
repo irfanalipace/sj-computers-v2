1a4d6d62a371d08ed79fd722596a0eeb47114e5b
Harris khan <harris.khan@99techbologies.co>
2023-05-23 16:14:28 +0500

    square fixed

--- app/Http/Controllers/Api/Square/SquareController.php
+++ app/Http/Controllers/Api/Square/SquareController.php
@@ -53,11 +53,11 @@ class SquareController extends BaseController
             
             $idempotencyKey = uniqid();
             // Clear the all cache
-            Artisan::call('optimize:clear');
+            // Artisan::call('optimize:clear');
             // Clear the application cache
-            Artisan::call('cache:clear');
+            // Artisan::call('cache:clear');
             // Clear the configuration cache           
-            Artisan::call('config:clear');
+            // Artisan::call('config:clear');
 
             //create customer || retrieve customer if already added
             if (auth()->user()->square_cus_id == null) {                
