b7c548624d84e5928b7432e19f27f30969f4f4e4
Irfan Ali <irfan.ali@99technologies.co>
2023-05-23 14:21:25 +0100

    23/05/2023 6.20pm

--- src/views/components/Policy/PolicyComponets.js
+++ src/views/components/Policy/PolicyComponets.js
@@ -101,7 +101,7 @@ const PolicyComponets = () => {
                     </div>
                    
                         </div>
-                    <div className="col-lg-8 col-md-10 col-sm-12 text-color-all-text text-dev-section-dev" >
+                    <div className="col-lg-8 col-md-10 col-sm-12 text-color-all-text text-dev-section-dev " style={{marginBottom:'44px'}}>
                         <div>
                         <p  className="data-ul-list-itel-paragraph" style={{color:'white'}}>Items shipped from sjcomputers.us, including SJ Computers Warehouse, can be returned within 30 days of delivery, with some exceptions</p>
                             <ul className="policy-item-data">
