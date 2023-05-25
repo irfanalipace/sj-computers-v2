de626bbb1e18bb41b5158e7a455a7aea90fc708a
Harris khan <harris.khan@99techbologies.co>
2023-05-25 12:11:33 +0500

    fix inventory now user can search with asin and sku

--- app/Http/Controllers/Api/InventoryController.php
+++ app/Http/Controllers/Api/InventoryController.php
@@ -17,18 +17,17 @@ class InventoryController extends BaseController
     public function getInventory(GetInventoryRequest $request)
     {
         try {
-           
+
             $search = $request->search;
             if (preg_match('/^B0/', $search)) {
                 // Value starts with "B0", treat it as ASIN
                 $type = StatusEnum::ASIN;
-                
             } elseif (strpos($search, '-') !== false) {
                 // Value contains a hyphen "-", treat it as SKU
                 $type = StatusEnum::SKU;
             }
-            
-            $inventory = $this->getAmazonInventory(null,$type, $search);
+
+            $inventory = $this->getAmazonInventory(null, $type, $search);
 
             return $this->sendResponse([$inventory], 'Amazon product list');
         } catch (Exception $e) {
@@ -40,22 +39,28 @@ class InventoryController extends BaseController
     public function ActionPerform(ActionPerfomRequest $request)
     {
         try {
-            $inventory = $this->getAmazonInventory(null,$request->search);
+            $search = $request->search;
+            if (preg_match('/^B0/', $search)) {
+                // Value starts with "B0", treat it as ASIN
+                $type = StatusEnum::ASIN;
+            } elseif (strpos($search, '-') !== false) {
+                // Value contains a hyphen "-", treat it as SKU
+                $type = StatusEnum::SKU;
+            }
+            $inventory = $this->getAmazonInventory(null,$type, $search);
 
             if ($inventory['status']) {
 
-                $this->updateAmazonInventory($inventory, $request->quantity,$request->action);
+                $this->updateAmazonInventory($inventory, $request->quantity, $request->action);
 
-                $amazonInventory =  $this->getAmazonInventory(null,$request->search);
+                $amazonInventory =  $this->getAmazonInventory(null, $request->search);
 
                 return $this->sendResponse([$amazonInventory], 'Amazon product list');
-            }else{
+            } else {
                 $this->sendError(["Error", "Sku not found."]);
             }
-           
         } catch (Exception $e) {
             $this->sendError(["Error", "Something went wrong." . $e]);
         }
     }
-
 }
