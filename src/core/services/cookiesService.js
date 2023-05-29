76c6d5cf501f7f6d57ce898c886d048ddbb06529
haroon.mukhtar <haroon.mukhtar@99technologies.co>
2023-05-29 13:38:29 +0500

    added try catch to avoid errors

--- src/core/services/cookiesService.js
+++ src/core/services/cookiesService.js
@@ -1,13 +1,17 @@
 // Function sets user latest visit date in cookie
 export function setUserTracking() {
-    checkLastVisitDate();
-    const lastVisitDate = getCurrentDate();
-    const expirationDate = new Date();
-    expirationDate.setDate(expirationDate.getDate() + 365);
-    const cookie = `${encodeURIComponent("visited")}=${encodeURIComponent(
-        lastVisitDate
-    )};expires=${expirationDate.toUTCString()};path=/`;
-    document.cookie = cookie;
+    try {
+        checkLastVisitDate();
+        const lastVisitDate = getCurrentDate();
+        const expirationDate = new Date();
+        expirationDate.setDate(expirationDate.getDate() + 365);
+        const cookie = `${encodeURIComponent("visited")}=${encodeURIComponent(
+            lastVisitDate
+        )};expires=${expirationDate.toUTCString()};path=/`;
+        document.cookie = cookie;
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 const dateToCheck = "2023-05-30";
@@ -21,24 +25,30 @@ const actionToPerform = () => {
 
 // This Function checks when did user last visited our website
 function checkLastVisitDate() {
-    const cookies = document.cookie.split(";");
-    for (let i = 0; i < cookies.length; i++) {
-        const cookie = cookies[i]?.trim();
-        if (cookie?.startsWith(`visited=`)) {
-            const cookieValue = cookie?.substring("visited".length + 1);
-            let decodedDate = decodeURIComponent(cookieValue);
+    try {
+        const cookies = document.cookie.split(";");
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i]?.trim();
+            if (cookie?.startsWith(`visited=`)) {
+                const cookieValue = cookie?.substring("visited".length + 1);
 
-            //  write all the conditions and opeartion to perform on conditions below
+                let decodedDate = decodeURIComponent(cookieValue);
 
-            if (
-                new Date(decodedDate).getTime() >=
-                new Date(dateToCheck).getTime()
-            ) {
-                actionToPerform();
+                //  write all the conditions and opeartion to perform on conditions below
+
+                if (
+                    new Date(decodedDate).getTime() >=
+                    new Date(dateToCheck).getTime()
+                ) {
+                    actionToPerform();
+                }
             }
         }
+        return false;
+    } catch (error) {
+        console.log(error);
     }
-    return false;
+    const cookies = document.cookie.split(";");
 }
 
 function getCurrentDate() {
