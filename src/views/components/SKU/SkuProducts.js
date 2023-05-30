db2debd4e347656d5a5f37e4bfecf531b4808481
haroon.mukhtar <haroon.mukhtar@99technologies.co>
2023-05-30 14:02:24 +0500

    added search on sku page

--- src/views/components/SKU/SkuProducts.js
+++ src/views/components/SKU/SkuProducts.js
@@ -5,127 +5,87 @@ import TableCell from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
-import { Button } from "@mantine/core";
+import Pagination from "@mui/material/Pagination";
+import { TextField, FormControl } from "@mui/material";
+
 import Paper from "@mui/material/Paper";
-import { productsApi } from "@api/products";
+import { productsApi, filterProductsApi } from "@api/products";
 
 export const SkuProducts = () => {
     const [data, setData] = useState([]);
     const [currentPage, setCurrentPage] = useState(0);
-    const [totalRecords, setTotalRecords] = useState(40);
+    const [pageCount, setPageCount] = useState(0);
+    const [search, setSearch] = useState("");
+    const [isSearchActive, setIsSearchActive] = useState(false);
 
     const perPage = 12; // Adjust the number of items per page as needed
 
     useEffect(() => {
-        fetchProducts(currentPage + 1);
+        fetchProducts(null, currentPage + 1);
     }, []);
 
-    const fetchProducts = async (page) => {
+    const fetchProducts = async (e, page) => {
         try {
             let response = await productsApi(page, perPage);
             setData(response.data.data);
-            setTotalRecords(response.data.total);
             setCurrentPage(response.data.current_page);
+            setPageCount(response.data.last_page);
         } catch (error) {
             console.log("error", error);
         }
     };
 
-    const pageCount = Math.ceil(totalRecords / perPage);
-
-    const goToPage = (page) => {
-        setCurrentPage(page);
-        fetchProducts(page);
-    };
-
-    const nextPage = () => {
-        if (currentPage < pageCount) {
-            // setCurrentPage((prevPage) => prevPage + 1);
-            setCurrentPage(currentPage + 1);
-            console.log(currentPage, "current after next");
-            console.log(currentPage + 1, "+1 after next");
-            fetchProducts(currentPage + 1);
-        }
-    };
-
-    const previousPage = () => {
-        if (currentPage > 1) {
-            setCurrentPage(currentPage - 1);
-            fetchProducts(currentPage - 1);
-        }
-    };
-
-    const renderPagination = () => {
-        const pages = [];
-
-        // Case 1: More than 5 pages
-        if (pageCount > 5) {
-            for (let i = 1; i <= 2; i++) {
-                pages.push(
-                    <Button
-                        style={{
-                            background:
-                                currentPage === i ? "#198754" : "outline",
-                            margin: "0px 5px",
-                        }}
-                        key={i}
-                        onClick={() => goToPage(i)}
-                        variant={currentPage === i ? "filled" : "outline"}
-                    >
-                        {i}
-                    </Button>
-                );
+    const handleSearch = async (e, page = 1) => {
+        const filter = {
+            name: search,
+            page,
+            per_page: perPage,
+        };
+        try {
+            setIsSearchActive(true);
+            if (search) {
+                let response = await filterProductsApi(filter);
+                setData(response.data.data);
+                setCurrentPage(response.data.current_page);
+                setPageCount(response.data.last_page);
+            } else {
+                fetchProducts(null, 1);
+                setIsSearchActive(false);
             }
-            pages.push(<span key="dots">...</span>);
-            pages.push(
-                <Button
-                    style={{
-                        background:
-                            currentPage === pageCount ? "#198754" : "outline",
-                        margin: "0px 5px",
-                    }}
-                    key={pageCount}
-                    onClick={() => goToPage(pageCount)}
-                    variant={currentPage === pageCount ? "filled" : "outline"}
-                >
-                    {pageCount}
-                </Button>
-            );
+        } catch (error) {
+            console.log("error", error);
         }
-        // Case 2: Less than or equal to 5 pages
-        else {
-            for (let i = 1; i <= pageCount; i++) {
-                pages.push(
-                    <Button
-                        style={{
-                            background:
-                                currentPage === i ? "#198754" : "outline",
-                            margin: "0px 5px",
-                        }}
-                        key={i}
-                        onClick={() => goToPage(i)}
-                        variant={currentPage === i ? "filled" : "outline"}
-                    >
-                        {i}
-                    </Button>
-                );
-            }
-        }
-
-        return <div>{pages}</div>;
     };
 
     return (
         <div>
             <div className="product-info py-4">
                 <h3 className="my-3">Products</h3>
+
+                <div className="seach-input-sku">
+                    <FormControl
+                        className="search-field"
+                        sx={{ m: 1, minWidth: 500 }}
+                    >
+                        <TextField
+                            id="outlined-basic"
+                            label="Search by name"
+                            variant="outlined"
+                            value={search}
+                            onChange={(e) => setSearch(e.target.value)}
+                        />
+                    </FormControl>
+                    <button onClick={handleSearch} className="search-btn">
+                        Search
+                    </button>
+                </div>
                 <TableContainer component={Paper}>
                     <Table aria-label="products table">
                         <TableHead>
                             <TableRow>
                                 <TableCell>Name</TableCell>
-                                <TableCell align="right">ASIN</TableCell>
-                                <TableCell align="right">SKU</TableCell>
+                                <TableCell align="center">ASIN</TableCell>
+                                <TableCell align="center">SKU</TableCell>
                                 <TableCell align="right">Quantity</TableCell>
                             </TableRow>
                         </TableHead>
@@ -137,15 +97,19 @@ export const SkuProducts = () => {
                                         "&:last-child td, &:last-child th": {
                                             border: 0,
                                         },
+
+                                        "& td:first-of-type ": {
+                                            maxWidth: "400px",
+                                        },
                                     }}
                                 >
                                     <TableCell align="left" scope="row">
                                         {row.name}
                                     </TableCell>
-                                    <TableCell align="right">
+                                    <TableCell align="center">
                                         {row.asin}
                                     </TableCell>
-                                    <TableCell align="right">
+                                    <TableCell align="center">
                                         {row.sku}
                                     </TableCell>
                                     <TableCell align="right">
@@ -160,23 +124,11 @@ export const SkuProducts = () => {
                     className="my-2"
                     style={{ display: "flex", justifyContent: "flex-end" }}
                 >
-                    <Button
-                        style={{ background: "#198754", marginRight: "2px" }}
-                        onClick={previousPage}
-                        disabled={currentPage === 1}
-                        className="toggle-button-table-data"
-                    >
-                        Previous
-                    </Button>
-                    {renderPagination()}
-                    <Button
-                        style={{ background: "#198754" }}
-                        onClick={nextPage}
-                        disabled={currentPage === pageCount}
-                        className="toggle-button-table-data"
-                    >
-                        Next
-                    </Button>
+                    <Pagination
+                        count={pageCount}
+                        page={currentPage}
+                        onChange={isSearchActive ? handleSearch : fetchProducts}
+                    />
                 </div>
             </div>
         </div>
