bb799af2f57c4f1f2811770cfec877387f465952
haroon.mukhtar <haroon.mukhtar@99technologies.co>
2023-06-05 18:42:08 +0500

    hard reload on page refresh

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -35,7 +35,14 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
         You need to enable JavaScript to run this app.
 
     </noscript>
+    <script>
+        let times = window.localStorage.getItem('cacheCleared')
+        if(!times){
+                    window.location.reload(true);
+                    window.localStorage.setItem('cacheCleared', times +1)
+        }  
 
+    </script>
 
     <script src={{ asset(mix('/js/app.js')) }}></script>
 
