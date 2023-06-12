2e1a8c22a8cab58a1b7476bab84ac3f8aba36038
Irfan Ali <irfan.ali@99technologies.co>
2023-06-12 06:50:11 +0100

    update

---
+++ src/views/pages/Blog/BlogSingle.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import SingleBlog from '@components/Blog/SingleBlog/SingleBlog'
+const BlogSingle = () => {
+  return (
+    <div>
+        <SingleBlog />
+    </div>
+  )
+}
+
+export default BlogSingle
