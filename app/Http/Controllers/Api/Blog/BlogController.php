eb14c46f5b29af6caba5d5d5179a1cd958ccbb6e
mudassar <mudassarprogrammer@gmail.com>
2023-06-19 11:01:08 +0500

    set publich date in query

--- app/Http/Controllers/Api/Blog/BlogController.php
+++ app/Http/Controllers/Api/Blog/BlogController.php
@@ -6,12 +6,13 @@ namespace App\Http\Controllers\Api\Blog;
 use App\Http\Controllers\Api\BaseController;
 use App\Models\Blog;
 use App\Models\Brand;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class BlogController extends BaseController
 {
     public function getList(request $request){
-        $data= Blog::all();
+        $data= Blog::whereDate('publish_date', '<=', Carbon::now()->toDateString());
         return $this->sendResponse($data);
     }
 }
