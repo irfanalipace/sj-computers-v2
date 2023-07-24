482e57d5fe4c1cd5d6763238da2419fe1af290f4
mudassar <mudassarprogrammer@gmail.com>
2023-07-24 15:14:05 +0500

    set category product

---
+++ database/migrations/2023_07_24_101229_create_categories_products_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoriesProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories_products', function (Blueprint $table) {
+            $table->foreignId('category_id');
+            $table->foreignId('product_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories_products');
+    }
+}
