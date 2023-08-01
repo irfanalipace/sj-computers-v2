30819b4c6a04c81eb7ed7052eebfb03ab97af852
Haroon_Mukhtar <haroon.mukhtar@99technologies.co>
2023-08-01 16:00:14 +0500

    fixes

--- src/views/components/Header/MobileSearch/MobileSearch.js
+++ src/views/components/Header/MobileSearch/MobileSearch.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, lazy, Suspense } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
