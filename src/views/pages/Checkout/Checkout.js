390b316b7fe3e66a251a697de08aef8f300fc18c
Irfan Ali <irfan.ali@99technologies.co>
2023-08-01 13:25:52 +0100

    updates

--- src/views/pages/Checkout/Checkout.js
+++ src/views/pages/Checkout/Checkout.js
@@ -82,17 +82,12 @@ export default function Checkout() {
                                     </Link>
                                 </div>
                                 <div className="items-number">
-                                    {isAuthenticated ? (
+                                    
                                         <h3>
                                             Checkout (
                                             {checkoutDetails.total_items} items)
                                         </h3>
-                                    ) : (
-                                        <h3>
-                                           Guest Checkout (
-                                            1 items)
-                                        </h3>
-                                    )}
+                                   
                                 </div>
                             </div>
                         </div>
@@ -161,7 +156,7 @@ export default function Checkout() {
                                             <ShippingMethod />
                                         </div>
                                     </div>
-                                    {!isAuthenticated && (
+                                    {/* {!isAuthenticated && (
                                         <div>
                                             <div>
                                                 <Discount
@@ -178,7 +173,7 @@ export default function Checkout() {
                                                 />
                                             </div>
                                         </div>
-                                    )}
+                                    )} */}
 
                                     {/* <div className="shipping-method-component-wrapper">
                                         <ShippingMethod />
