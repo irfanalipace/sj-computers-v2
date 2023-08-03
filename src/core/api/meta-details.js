1c06b919d1d5388cfb321c2d000750d8535d3ad7
Irfan Ali <irfan.ali@99technologies.co>
2023-08-03 09:18:35 +0100

    update

---
+++ src/core/api/meta-details.js
@@ -0,0 +1,19 @@
+
+import ApiService from "@services/apiService";
+
+export function metaDetailsApi() {
+    return new Promise((resolve, reject) => {
+        ApiService.get(`/meta_detail`)
+            .then((response) => {
+                console.print(
+                    "file: meta-details.js | metaDetailsApi| response",
+                    response
+                );
+                resolve(response);
+            })
+            .catch((e) => {
+                console.print("Console Log: : error meta", e);
+                reject(e);
+            });
+    });
+}
