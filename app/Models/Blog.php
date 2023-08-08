391760ab4d6342cc39f7821345aa4bbb1d54e366
mudassar <mudassarprogrammer@gmail.com>
2023-08-08 11:55:47 +0500

    set code

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -18,4 +18,10 @@ class Blog extends Model
     public function categories(){
         return $this->belongsToMany(Category::class,'blogs_categories');
     }
+
+    public function getThumbnailImageAttribute($value)
+    {
+        return url('storage/'.$value);
+    }
+
 }
