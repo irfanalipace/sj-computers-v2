80f37f73a2923d314bfac2e919be9c4d633da898
irfan.ali123 <irfan.ali@99technologies.co>
2023-08-17 01:21:06 -1100

    fgdfgdf

--- src/views/components/PageWrapper.js
+++ src/views/components/PageWrapper.js
@@ -93,26 +93,14 @@
 
 // // export default PageWrapper;
 
-<<<<<<<< HEAD:src/views/components/PageWrapper.js
-
-import React, { useEffect, useState,Suspense } from 'react';
-import { Helmet } from 'react-helmet';
-import { metaDetailsApi } from '../core/api/meta-details';
-import Loader from "@common/LoaderComponent/LoaderComponent";
-========
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, Suspense } from "react";
 import { Helmet } from "react-helmet";
 import { metaDetailsApi } from "../../../core/api/meta-details";
-
->>>>>>>> origin/test-merge-3:src/views/components/PageWrapper/PageWrapper.js
+import Loader from "@common/LoaderComponent/LoaderComponent";
 const PageWrapper = (props) => {
     const [meta, setMeta] = useState({ title: "", description: "" });
     const url = window.location.href;
 
-<<<<<<<< HEAD:src/views/components/PageWrapper.js
-  const [meta, setMeta] = useState({ title: '', description: '' });
-  const url = window.location.href;
-========
     useEffect(() => {
         if (url)
             metaDetailsApi(url)
@@ -127,7 +115,7 @@ const PageWrapper = (props) => {
                             "Buy ALL Brands Touch Screen Laptops, Gaming Desktop, Business Computer, Best BTO and more We looked at many companies, including Dell and Apple.",
                     });
                 });
-    }, []);
+    }, [url]);
 
     // useEffect(() => {
     //   document.title = meta.title;
@@ -136,10 +124,9 @@ const PageWrapper = (props) => {
     //     descriptionMetaTag.setAttribute('content', meta.description);
     //   }
     // }, [meta]);
->>>>>>>> origin/test-merge-3:src/views/components/PageWrapper/PageWrapper.js
 
     return (
-        <>
+        <Suspense fallback={<Loader />}>
             {meta.title && (
                 <Helmet>
                     <title>{meta.title}</title>
@@ -147,47 +134,10 @@ const PageWrapper = (props) => {
                 </Helmet>
             )}
 
-<<<<<<<< HEAD:src/views/components/PageWrapper.js
-    metaDetailsApi(url)
-      .then((response) => {
-     
-        const { title, description } = response?.data;
-        setMeta({ title, description });
-      })
-      .catch((error) => {
-        console.error('API Error:', error);
-        setMeta({ title: 'SJ Computers', description:'' });
-      });
-
-  }, [url]);
-
-  // useEffect(() => {
-  //   document.title = meta.title;
-  //   const descriptionMetaTag = document.querySelector('meta[name="description"]');
-  //   if (descriptionMetaTag) {
-  //     descriptionMetaTag.setAttribute('content', meta.description);
-  //   }
-  // }, [meta]);
-
-
-
-  return (
-    <Suspense fallback={<Loader />}>
-      {meta.title && (
-        <Helmet>
-          <title>{meta.title}</title>
-          <meta name="description" content={meta.description} />
-        </Helmet>
-      )}
-
-      {props.children}
-    </Suspense>
-  );
-========
             {props.children}
-        </>
+        </Suspense>
     );
->>>>>>>> origin/test-merge-3:src/views/components/PageWrapper/PageWrapper.js
 };
 
 export default PageWrapper;
+
