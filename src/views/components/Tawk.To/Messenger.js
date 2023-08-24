43ca127d021c59a2637a1009d09325a605c5892d
irfan.ali123 <irfan.ali@99technologies.co>
2023-08-23 19:03:20 -1100

    update

--- src/views/components/Tawk.To/Messenger.js
+++ src/views/components/Tawk.To/Messenger.js
@@ -2,7 +2,7 @@ import TawkMessengerReact from "@tawk.to/tawk-messenger-react";
 
 const TawkTo = () => {
     return (
-        <div className="tawk-to-container">
+        <div className="tawk-to-container" >
             <TawkMessengerReact
                 propertyId="5d3165689b94cd38bbe8276d"
                 widgetId="default"
