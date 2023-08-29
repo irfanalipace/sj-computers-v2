b4344e917dd0364b74821b59b71d2edcff5e479a
mudassar <mudassarprogrammer@gmail.com>
2023-08-29 17:01:31 +0500

    set sitemap

--- app/Console/Commands/GenerateSiteMap.php
+++ app/Console/Commands/GenerateSiteMap.php
@@ -58,9 +58,9 @@ class GenerateSiteMap extends Command
 
 
         $generalRoutes = [
-            '/pages.xml',
-            '/categories.xml',
-            '/blogs.xml',
+            '/sitemap/pages.xml',
+            '/sitemap/categories.xml',
+            '/sitemap/blogs.xml',
         ];
 
         foreach ($generalRoutes as $route) {
