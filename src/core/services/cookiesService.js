16e0a36c13e4b701e410f56dd685911e937d7233
Haroon_Mukhtar <haroon.mukhtar@99technologies.co>
2023-09-07 17:45:06 +0500

    added user cart timeout feature and refactored cookies service code

--- src/core/services/cookiesService.js
+++ src/core/services/cookiesService.js
@@ -1,48 +1,73 @@
-// Function sets user latest visit date in cookie
-export function setUserTracking() {
-    try {
-        checkLastVisitDate();
-        const lastVisitDate = getCurrentDate();
-        const expirationDate = new Date();
-        expirationDate.setDate(expirationDate.getDate() + 365);
-        const cookie = `${encodeURIComponent("visited")}=${encodeURIComponent(
-            lastVisitDate
-        )};expires=${expirationDate.toUTCString()};path=/`;
-        document.cookie = cookie;
-    } catch (error) {
-        console.print(error);
-    }
-}
-
-const dateToCheck = "2023-06-07";
-
-const actionToPerform = () => {};
-
-// This Function checks when did user last visited our website
-function checkLastVisitDate() {
-    try {
+const cookiesService = {
+    setCookie: (name, value, days) => {
+        const expires = new Date();
+        expires.setTime(expires.getTime() + days * 24 * 60 * 60 * 1000);
+        document.cookie = `${name}=${value};expires=${expires.toUTCString()};path=/`;
+    },
+    getCookie: (name) => {
+        const cookieName = `${name}=`;
         const cookies = document.cookie.split(";");
         for (let i = 0; i < cookies.length; i++) {
-            const cookie = cookies[i]?.trim();
-            if (cookie?.startsWith(`visited=`)) {
-                const cookieValue = cookie?.substring("visited".length + 1);
-                let decodedDate = decodeURIComponent(cookieValue);
-
-                //  write all the conditions and opeartion to perform on conditions below
-
-                if (
-                    new Date(decodedDate).getTime() <=
-                    new Date(dateToCheck).getTime()
-                ) {
-                    actionToPerform();
-                }
+            let cookie = cookies[i].trim();
+            if (cookie.indexOf(cookieName) === 0) {
+                return cookie.substring(cookieName.length, cookie.length);
             }
         }
-        return false;
-    } catch (error) {
-        console.print(error);
-    }
-}
+        return null;
+    },
+    deleteCookie: (name) => {
+        document.cookie = `${name}=;expires=Thu, 01 Jan 1970 00:00:00 UTC;path=/;`;
+    },
+    setUserTracking: function () {
+        // Function sets the user's latest visit date in a cookie
+
+        try {
+            this.checkLastVisitDate();
+            const lastVisitDate = getCurrentDate();
+            const expirationDate = new Date();
+            expirationDate.setDate(expirationDate.getDate() + 365);
+            const cookie = `${encodeURIComponent(
+                "visited"
+            )}=${encodeURIComponent(
+                lastVisitDate
+            )};expires=${expirationDate.toUTCString()};path=/`;
+            document.cookie = cookie;
+        } catch (error) {
+            console.error(error); // Changed from console.print to console.error
+        }
+    },
+    checkLastVisitDate: function () {
+        // This Function checks when the user last visited our website
+        try {
+            const cookies = document.cookie.split(";");
+            for (let i = 0; i < cookies.length; i++) {
+                const cookie = cookies[i]?.trim();
+                if (cookie?.startsWith(`visited=`)) {
+                    const cookieValue = cookie?.substring("visited".length + 1);
+                    let decodedDate = decodeURIComponent(cookieValue);
+
+                    if (
+                        new Date(decodedDate).getTime() <=
+                        new Date(dateToCheck).getTime()
+                    ) {
+                        this.actionToPerform();
+                    }
+                }
+            }
+            return false;
+        } catch (error) {
+            console.error(error); // Changed from console.print to console.error
+        }
+    },
+    actionToPerform: function () {
+        // Define the action to perform here
+    },
+    init: function () {
+        this.setUserTracking();
+    },
+};
+
+const dateToCheck = "2023-06-07";
 
 function getCurrentDate() {
     const currentDate = new Date();
@@ -52,3 +77,5 @@ function getCurrentDate() {
 
     return `${year}-${month}-${day}`;
 }
+
+export default cookiesService;
