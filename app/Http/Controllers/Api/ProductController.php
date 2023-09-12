aa1607b7e84376ae204ae083f33288e8b49a3231
mahnoor <mahnoor@99technologies.co>
2023-09-12 12:39:00 +0500

    Fixed product filter method

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -148,7 +148,7 @@ class ProductController extends BaseController
 
             foreach ($filters as $filter) {
 
-                $filter = json_encode($filter, true);
+//                $filter = json_encode($filter, true);
                 $filter = json_decode($filter, true);
 
                 $key = $filter['key'] ?? '';
