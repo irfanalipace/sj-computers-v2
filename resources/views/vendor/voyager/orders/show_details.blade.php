5f9b574306e5aa6a75c553ff00654e5def796121
mahnoor <mahnoor@99technologies.co>
2023-11-06 10:39:31 +0500

    Fixed order details

--- resources/views/vendor/voyager/orders/show_details.blade.php
+++ resources/views/vendor/voyager/orders/show_details.blade.php
@@ -17,7 +17,7 @@
                             <h3 class="panel-title">name</h3>
                     </div>
                     <div class="panel-body" style="padding-top:0;">
-                        <p>{{$invoiceOrder->user->shippingAddress->full_name ?? '' }}</p>
+                        <p>{{$invoiceOrder->user->shippingAddress->full_name ?? $invoiceOrder->guest->full_name }}</p>
                     </div>
                     <hr style="margin:0;">
 
@@ -25,7 +25,7 @@
                         <h3 class="panel-title">Phone Number</h3>
                     </div>
                     <div class="panel-body" style="padding-top:0;">
-                        <p>{{$invoiceOrder->user->shippingAddress->phone_number ?? ''}}</p>
+                        <p>{{$invoiceOrder->user->shippingAddress->phone_number ?? $invoiceOrder->guest->phone_number}}</p>
                     </div>
                     <hr style="margin:0;">
 
@@ -33,7 +33,7 @@
                         <h3 class="panel-title">Email</h3>
                     </div>
                     <div class="panel-body" style="padding-top:0;">
-                        <p>{{ $invoiceOrder->user->shippingAddress->email ?? ''}}</p>
+                        <p>{{ $invoiceOrder->user->shippingAddress->email ?? $invoiceOrder->guest->email}}</p>
                     </div>
                     <hr style="margin:0;">
 
@@ -41,7 +41,39 @@
                         <h3 class="panel-title">Address</h3>
                     </div>
                     <div class="panel-body" style="padding-top:0;">
-                        <p>{{ $invoiceOrder->user->shippingAddress->address ?? ''}}</p>
+                        <p>{{ $invoiceOrder->user->shippingAddress->address ?? $invoiceOrder->guest->address}}</p>
+                    </div>
+                    <hr style="margin:0;">
+
+                    <div class="panel-heading" style="border-bottom:0;">
+                        <h3 class="panel-title">Country</h3>
+                    </div>
+                    <div class="panel-body" style="padding-top:0;">
+                        <p>{{ $invoiceOrder->user->shippingAddress->country ?? $invoiceOrder->guest->country}}</p>
+                    </div>
+                    <hr style="margin:0;">
+
+                    <div class="panel-heading" style="border-bottom:0;">
+                        <h3 class="panel-title">City</h3>
+                    </div>
+                    <div class="panel-body" style="padding-top:0;">
+                        <p>{{ $invoiceOrder->user->shippingAddress->city ?? $invoiceOrder->guest->city}}</p>
+                    </div>
+                    <hr style="margin:0;">
+
+                    <div class="panel-heading" style="border-bottom:0;">
+                        <h3 class="panel-title">State</h3>
+                    </div>
+                    <div class="panel-body" style="padding-top:0;">
+                        <p>{{ $invoiceOrder->user->shippingAddress->state ?? $invoiceOrder->guest->state}}</p>
+                    </div>
+                    <hr style="margin:0;">
+
+                    <div class="panel-heading" style="border-bottom:0;">
+                        <h3 class="panel-title">Zipcode</h3>
+                    </div>
+                    <div class="panel-body" style="padding-top:0;">
+                        <p>{{ $invoiceOrder->user->shippingAddress->zip_code ?? $invoiceOrder->guest->zip_code}}</p>
                     </div>
                     <hr style="margin:0;">
 
