b25d1c90f0f7510bcf3b2396cbb0225612f39458
Haroon_Mukhtar <haroon.mukhtar@99technologies.co>
2023-11-07 15:09:46 +0500

    fix

--- src/core/utils/constants.js
+++ src/core/utils/constants.js
@@ -28,8 +28,13 @@ export const shippingMethods = [
 ];
 
 export const dummyCategories = [
-    { name: "ALL", id: null },
-    { name: "1", id: 1 },
-    { name: "2", id: 2 },
-    { name: "3", id: 3 },
+    { name: "ALL", id: null, slug: "" },
+    { name: "1", id: 1, slug: "1" },
+    { name: "2", id: 2, slug: "2" },
+    { name: "3", id: 3, slug: "3" },
+    { name: "category 4", id: 4, slug: "category-4" },
+    { name: "tablet", id: 5, slug: "tablet" },
+    { name: "monitor", id: 5, slug: "monitor" },
+    { name: "category 1", id: 6, slug: "category-1" },
+    { name: "category 2", id: 7, slug: "category-2" },
 ];
