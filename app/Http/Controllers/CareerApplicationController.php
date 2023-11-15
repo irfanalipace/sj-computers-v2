43f31703db3fed63f6412d2255ba964d46fdee50
mahnoor <mahnoor@99technologies.co>
2023-11-15 10:24:08 +0500

    Fixed career applications

--- app/Http/Controllers/CareerApplicationController.php
+++ app/Http/Controllers/CareerApplicationController.php
@@ -13,20 +13,18 @@ class CareerApplicationController extends Controller
     public function store(StoreCareerApplicationRequest $request)
     {
         if ($request->hasFile('resume')) {
-            $filename = $request->file('resume')->store('public/resume');
-            $user['file']['resume'] = str_replace('public/', '', $filename);
+            $user['resume'] = $request->file('resume')->storeAs('careers/resumes', time() . '_' . $request->resume->getClientOriginalName());
         }
         if ($request->hasFile('cover_letter')) {
-            $filename = $request->file('cover_letter')->store('public/cover_letter');
-            $user['file']['cover_letter'] = str_replace('public/', '', $filename);
+            $user['cover_letter'] = $request->file('cover_letter')->storeAs('careers/cover_letters', time() . '_' . $request->cover_letter->getClientOriginalName());
         }
 
+        $careerApplication = CareerApplication::query()->create($request->all());
         $files = [
-            $user['file']['resume'],
-            $user['file']['cover_letter'],
+            storage_path('app/' . $user['resume']),
+            storage_path('app/' . $user['cover_letter']),
         ];
 
-        $careerApplication = CareerApplication::query()->create($request->all());
         Mail::to(['99tech.ai@gmail.com', 'joe@sjcomputersmn.com'])->send(new CareerApplicationMail($careerApplication->job_title, $files));
 
         return response()->json([
