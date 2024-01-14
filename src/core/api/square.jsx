d754ec50e5ccd654d8044ec9d4020a4444454363
mudassar <mudassar.irshad@99technologies.co>
2024-01-14 17:45:37 +0500

    SEt code

--- src/core/api/square.jsx
+++ src/core/api/square.jsx
@@ -2,7 +2,7 @@ import ApiService from "@services/apiService";
 
 export function sendTokenApi(data) {
     return new Promise((resolve, reject) => {
-        ApiService.post(`/paypal`, data)
+        ApiService.post(`/square-charge`, data)
             .then((response) => {
                 console.print(
                     "file: square.js | sendTokenApi| response",
