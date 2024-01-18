2bfbe3f55dfb42a8e13ad5734edf46ed01a16b03
umarzahir <umar.zahir@99technologies.co>
2024-01-18 16:43:39 +0500

    google places autocomplete

---
+++ src/views/components/common/GooglePlaces/AutoComplete.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Autocomplete from "react-google-autocomplete";
+
+export default function AutoComplete({ onPlaceSelected, ...rest }) {
+    return (
+        <Autocomplete
+            {...rest}
+            id="address"
+            name="address"
+            options={{
+                fields: ["ALL"],
+                types: ["address"],
+                componentRestrictions: { country: "us" },
+            }}
+            apiKey={import.meta.env.VITE_APP_GOOGLE_MAPS_API_KEY}
+            onPlaceSelected={onPlaceSelected}
+        />
+    );
+}
