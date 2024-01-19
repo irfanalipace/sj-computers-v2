c3d1d4cf9c4a535d435057af3f2781c60cfc4c9b
irfanali99tech <irfan.ali@99technologies.com>
2024-01-19 18:20:59 +0500

    add review page updated

--- src/views/components/AddVideoDialogBox/AddVideoDialogBox.jsx
+++ src/views/components/AddVideoDialogBox/AddVideoDialogBox.jsx
@@ -1,21 +1,180 @@
-// AddVideoDialogBox.js
-import React from 'react';
-import './AddVideoDialogBox.css';
-
+import React, { useState, useEffect } from "react";
+import { Button, Modal } from "react-bootstrap";
+import fileService from "../../../core/utils/fileServices";
+import "./AddVideoDialogBox.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUpload } from "@fortawesome/free-solid-svg-icons";
+import { Photo } from "@material-ui/icons";
+import productphoto from '../../../assets/images/product/image20.png'
 const AddVideoDialogBox = ({ onClose }) => {
-  const handleDialogClick = (e) => {
-    // Prevent the click event from propagating to the parent, so it doesn't close the dialog
+  const [temporaryFiles, setTemporaryFiles] = useState([]);
+  const [dragging, setDragging] = useState(false);
+  const [errors, setErrors] = useState([]);
+
+  const deletingFile = (e, file) => {
     e.stopPropagation();
+    const filteredFiles = temporaryFiles.filter((f) => f !== file);
+    setTemporaryFiles(filteredFiles);
   };
 
+  const downloadFile = (e, file) => {
+    e.stopPropagation();
+    if (file.id) {
+      window.open(file?.file_path);
+    } else {
+      window.open(URL.createObjectURL(file));
+    }
+  };
+
+  const handleSave = () => {
+    // Implement your save logic here
+    console.log("Save logic:", temporaryFiles);
+    onClose();
+  };
+
+  const dragOver = (e) => {
+    e.preventDefault();
+    setDragging(true);
+  };
+
+  const fileDrop = (e) => {
+    e.preventDefault();
+    setDragging(false);
+    fileService.handleFileDrag(
+      e,
+      ["image/*", "video/*"],
+      callBack,
+      1024 * 1024 * 10
+    );
+  };
+
+  const onFileUpload = (e) => {
+    fileService.handleFileInputChange(
+      e,
+      ["image/*", "video/*"],
+      callBack,
+      1024 * 1024 * 10
+    );
+  };
+
+  const callBack = (validFiles, errors) => {
+    setTemporaryFiles((prev) => [...prev, ...validFiles]);
+    setErrors(errors);
+  };
+
+  useEffect(() => {
+    const handleListener = (event) => {
+      fileService.handlePaste(
+        event,
+        ["image/*", "video/*"],
+        callBack,
+        1024 * 1024 * 10
+      );
+    };
+    document.addEventListener("paste", handleListener);
+
+    return () => {
+      document.removeEventListener("paste", handleListener);
+    };
+  }, []);
+
+  const photoLibrary = [
+    { src: {productphoto}, alt: "Photo 1" },
+    { src: {productphoto}, alt: "Photo 2" },
+    { src: {productphoto}, alt: "Photo 3" },
+    { src: {productphoto}, alt: "Photo 4" },
+    { src: {productphoto}, alt: "Photo 5" },
+  ];
+
   return (
-    <div className='video-photo-overlay-dialog-box' onClick={onClose}>
-      <div className='dialog-content-video-camera-sections' onClick={handleDialogClick}>
-        <h4>
-         Add Video drop and photo
-        </h4>
-        {/* Add your dialog box content here */}
-      </div>
+    <div className="custome-model-data-add-video-file">
+      <Modal show={true} onHide={onClose} ClassName="custom-modal">
+        <Modal.Body>
+          <label
+            style={{ width: "465px" }}
+            htmlFor="file-input"
+            onDragOver={dragOver}
+            onDrop={fileDrop}
+          >
+            <input
+              className="custome-model-data-add-video-file"
+              id="file-input"
+              type="file"
+              multiple
+              style={{ display: "none" }}
+              onChange={onFileUpload}
+              accept="image/*, video/*"
+            />
+            <div
+              className="text-center border p-5"
+              style={{
+                borderColor: dragging ? "blue" : "#c3c3c3",
+                borderRadius: "12px",
+              }}
+            >
+              <div className="upload-file-icon-data">
+                <button>
+                  <FontAwesomeIcon icon={faUpload} />
+                </button>
+              </div>
+              <p className="mb-3">Drag & drop file to import</p>
+              <form>
+              <input type="file" id="myFile" name="filename" />
+                 </form>
+            
+        
+
+              <p className="text-muted" style={{ fontSize: "13px" }}>
+                Maximum file size 1MB. and file format: PNG or JPG
+              </p>
+            </div>
+          </label>
+
+          <div className="photo-library">
+            {photoLibrary.map((photo, index) => (
+               
+      
+                <div key={index} className="photo-item">
+                <img src={photo.src} alt={photo.alt} />
+              </div>
+        
+            ))}
+          </div>
+
+          <ul className="list-group mt-3">
+            {temporaryFiles.map((file, index) => (
+              <li
+                key={index}
+                className="list-group-item d-flex justify-content-between align-items-center"
+              >
+                {/* Display thumbnail or name of the file */}
+                gsgdfg
+                <div>
+                  <button
+                    variant="danger"
+                    onClick={(e) => deletingFile(e, file)}
+                  >
+                    Delete
+                  </button>
+                  <button
+                    variant="primary"
+                    className="ms-2"
+                    onClick={(e) => downloadFile(e, file)}
+                  >
+                    Download
+                  </button>
+                </div>
+              </li>
+            ))}
+          </ul>
+          {errors.length > 0 &&
+            errors.map((error, index) => (
+              <p key={index} className="text-danger mt-2">
+                {error}
+              </p>
+            ))}
+        </Modal.Body>
+      </Modal>
     </div>
   );
 };
--- src/views/components/Product/ProductReviews/ProductNewReviews.jsx
+++ src/views/components/Product/ProductReviews/ProductNewReviews.jsx
@@ -16,78 +16,85 @@ const ProductNewReviews = () => {
     const handleCloseDialogBox = () => {
         setDialogBoxOpen(false);
     };
-  return (
-    <div>
-      <div className="container add-new-review">
-        <div className="row">
-          <div className="col-md-3 col-sm-12">
-            <div>
-              <img src={img} alt="Product" />
-            </div>
-          </div>
-          <div className="col-md-9 col-sm-12">
-            <div className="row">
-              <div className="col-md-11">
-                <div className="review-heading">
-                  <h5>
-                    LG 24ML600M-B 24” Full HD IPS con 3 lados virtualmente sin
-                    bordes monitor con doble HDMI - Negro
-                  </h5>
-                </div>
-                <div className="sj-computer-tags">
-                  <p>SJ Computers</p>
-                </div>
-              </div>
-            </div>
-            <div className="rating-review-star">
-              <div>
-                <StarRatings
-                  rating={3.5}
-                  starRatedColor="rgb(232, 126, 36)"
-                  numberOfStars={5}
-                  name="rating"
-                  starDimension={"20px"}
-                  starSpacing={"0"}
-                />
-              </div>
-              <div className="check-rating-star-review">
-                <FontAwesomeIcon icon={faCheck} />
-              </div>
-              <div className="posted-policy-review">
-                <p>Posted publicly as</p>
-              </div>
-              <div className="check-rating-star-review-name">
-                <p>John Q. Smith | Clear</p>
-              </div>
-            </div>
+    return (
+        <div>
+            <div className="container add-new-review">
+                <div className="row">
+                    <div className="col-md-3 col-sm-12">
+                        <div>
+                            <img src={img} alt="Product" />
+                        </div>
+                    </div>
+                    <div className="col-md-9 col-sm-12">
+                        <div className="row">
+                            <div className="col-md-11">
+                                <div className="review-heading">
+                                    <h5>
+                                        LG 24ML600M-B 24” Full HD IPS con 3
+                                        lados virtualmente sin bordes monitor
+                                        con doble HDMI - Negro
+                                    </h5>
+                                </div>
+                                <div className="sj-computer-tags">
+                                    <p>SJ Computers</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="rating-review-star">
+                            <div>
+                                <StarRatings
+                                    rating={3.5}
+                                    starRatedColor="rgb(232, 126, 36)"
+                                    numberOfStars={5}
+                                    name="rating"
+                                    starDimension={"20px"}
+                                    starSpacing={"0"}
+                                />
+                            </div>
+                            <div className="check-rating-star-review">
+                                <FontAwesomeIcon icon={faCheck} />
+                            </div>
+                            <div className="posted-policy-review">
+                                <p>Posted publicly as</p>
+                            </div>
+                            <div className="check-rating-star-review-name">
+                                <p>John Q. Smith | Clear</p>
+                            </div>
+                        </div>
 
-            <div className="col-md-12">
-              <div className="text-area-rating-review-list">
-                <textarea
-                  name="text"
-                  placeholder="Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a  typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final  copy is available."
-                ></textarea>
-              </div>
-            </div>
+                        <div className="col-md-12">
+                            <div className="text-area-rating-review-list">
+                                <textarea
+                                    name="text"
+                                    placeholder="Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a  typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final  copy is available."
+                                ></textarea>
+                            </div>
+                        </div>
 
-            <div className="preview-button-review">
-              <button className="preview-product-list-button">Preview</button>{" "}
-              <button className="camera-button-review" onClick={handleDialogBox}>
-                {" "}
-                <FontAwesomeIcon icon={faCamera} /> Add Photos/Video 
-              </button>{" "}
-              <button className="submit-review-button">Submit</button>
+                        <div className="preview-button-review">
+                            <button className="preview-product-list-button">
+                                Preview
+                            </button>{" "}
+                            <button
+                                className="camera-button-review"
+                                onClick={handleDialogBox}
+                            >
+                                {" "}
+                                <FontAwesomeIcon icon={faCamera} /> Add
+                                Photos/Video
+                            </button>{" "}
+                            <button className="submit-review-button">
+                                Submit
+                            </button>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
+            {dialogBoxOpen && (
+                <AddVideoDialogBox onClose={handleCloseDialogBox} />
+            )}
         </div>
-      </div>
-      {
-        dialogBoxOpen && (
-            <AddVideoDialogBox  onClose={handleCloseDialogBox}/>
-        )
-      }
-    </div>
-  );
+    );
 };
 
 export default ProductNewReviews;
