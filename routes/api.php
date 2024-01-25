6609ba7a4ee6afbae7371339d148f531db473e87
Harris khan <harris.khan@99techbologies.co>
2024-01-25 15:48:25 +0500

    product-reviews: create separte api for total rating and total reviews according to frontend team

--- routes/api.php
+++ routes/api.php
@@ -268,4 +268,5 @@ Route::get('cancel', [PayPalController::class, 'paypalCancel'])->name('cancel');
 Route::get('get-product-reviews',[ReviewController::class,'index']);
 Route::get('get-protection-plans',[ProductController::class,'getProtectivePlan']);
 Route::get('get-product-details/{product_id}',[ReviewController::class,'getProductDetail']);
+Route::get('get-product-rating/{product_id}',[ReviewController::class,'getProductRate']);
 
