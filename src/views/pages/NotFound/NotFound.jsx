69c3705053b6d185fbeaaf8adad492640653928c
Haroon_Mukhtar <haroon.mukhtar@99technologies.co>
2024-02-01 14:59:31 +0500

    not found component

--- src/views/pages/NotFound/NotFound.jsx
+++ src/views/pages/NotFound/NotFound.jsx
@@ -1,21 +1,30 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Search from "../../components/Header/Search";
-import "./NotFound.css"
+import "./NotFound.css";
 export default function NotFound() {
     return (
-        <div className="notfound-page-design-set">
-            <div className="d-flex flex-column align-items-center justify-content-center text-center" style={{ height: '70vh', background: 'white' }}>
-      <h1 className="display-1 text-primary">404</h1>
-      <h2 className="display-4 text-dark">Oops!</h2>
-      <h4 className="mb-4">Page Not Found</h4>
-     <div><h5>Browse Products</h5></div>
-        <Search />
-     
-      <Link to="/" className="btn btn-primary btn-sm">
-        Go Back To Home Page
-      </Link>
-    </div>
+        <div className="notfound-page">
+            <div className="d-flex flex-column align-items-center justify-content-center text-center w-100">
+                <div className="notfound-page-container">
+                    <h1 className="display-1 mb-0">404</h1>
+                    <h2 className="display-4 text-dark">Oops!</h2>
+                    <h4 className="mb-4">Page Not Found</h4>
+                    <div>
+                        <h5 className="text-start">Browse Products:</h5>
+                    </div>
+                    <div className="d-flex justify-content-center">
+                        <Search />
+                    </div>
+
+                    <Link
+                        to="/"
+                        className="btn btn-success btn-sm mt-3 py-2 px-4"
+                    >
+                        Go To Home Page
+                    </Link>
+                </div>
+            </div>
         </div>
     );
 }
