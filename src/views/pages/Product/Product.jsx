bcfc10fc7252f7787e150660e223907905e83c82
Haroon_Mukhtar <haroon.mukhtar@99technologies.co>
2024-02-02 12:27:12 +0500

    fixed delete item from cartoverlay

--- src/views/pages/Product/Product.jsx
+++ src/views/pages/Product/Product.jsx
@@ -34,7 +34,6 @@ export default function Product() {
     const [searchParams] = useSearchParams();
 
     const { isLoading, product } = useProductData();
-    debugger;
 
     const breadcrumbRoutes = [
         {
