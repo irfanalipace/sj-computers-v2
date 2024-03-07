f52e9d4e83af392fdc48ac1ded0efffe314e19e9
Laraib Mahmood <laraib@99technologies.co>
2024-03-07 12:00:28 +0000

    Fixes/sc 460

--- src/views/components/homeproduct/productcategory/ProductsItemsList/FeaturedProducts.jsx
+++ src/views/components/homeproduct/productcategory/ProductsItemsList/FeaturedProducts.jsx
@@ -18,7 +18,7 @@ const FeaturedProducts = ({
       <div className='row featured-products mx-0'>
         {featured.map((category, index) => (
           <div key={index} className='col-12 col-sm-6 col-lg-3'>
-            <Link to={category.link} className='text-decoration-none'>
+            <Link to={category.redirectTo} className='text-decoration-none'>
               <div className='product-type-section'>
                 <h2 className='h4-heading category-name'>{category.name}</h2>
                 <div
