d2cddd5a6a48dfafc2f4e1b87579dec62f32cc63
Laraib Mahmood <laraib@99technologies.co>
2024-03-07 11:45:15 +0000

    review checkbox color changes

--- src/views/components/Catagory/CategorySidebar/ReviewFilter.jsx
+++ src/views/components/Catagory/CategorySidebar/ReviewFilter.jsx
@@ -1,7 +1,7 @@
-import { Stack, Typography } from '@mui/material';
+import { Checkbox, Stack, Typography } from '@mui/material';
 import React from 'react';
 import StarRatings from 'react-star-ratings';
-
+import './FilterbarLayout2.css';
 export default function ReviewFilter({ onChange }) {
   const reveiwData = [
     { label: '4.5 & up', value: 4.5 },
@@ -13,15 +13,32 @@ export default function ReviewFilter({ onChange }) {
     <>
       {reveiwData.map((reveiw, index) => {
         return (
-          <Stack key={index} direction={'row'} spacing={1} mb={0.7}>
+          <Stack key={index} direction={'row'} spacing={1} mb={0.7} mt={1}>
             <label
-              className='radio-container'
+              className='checkbox-container'
               htmlFor={reveiw.value.toString()}>
-              <input
+              <Checkbox
                 id={reveiw.value.toString()}
-                type='checkbox'
+                style={{
+                  color: ' #f2a742',
+                  padding: '0 !important',
+                }}
+                icon={<span style={unchecked} />}
+                checkedIcon={<span style={checked} />}
                 onChange={event => onChange(event, 'reveiw', reveiw)}
               />
+              {/* <input
+                id={reveiw.value.toString()}
+                type='checkbox'
+                style={{
+                  color: '#f2a742', // Default color
+                  padding: '0 !important',
+                  backgroundColor: ' #f2a742', // Background color when checked
+                }}
+                name={'s'}
+                onChange={event => onChange(event, 'reveiw', reveiw)}
+              /> */}
+              {/* <span className='radiomark '></span> */}
             </label>
             <StarRatings
               starDimension='18px'
@@ -42,3 +59,19 @@ export default function ReviewFilter({ onChange }) {
     </>
   );
 }
+const checked = {
+  backgroundColor: '#f2a742',
+  border: '1px solid #000',
+  borderRadius: '3px',
+  display: 'inline-block',
+  width: '13px',
+  height: '13px',
+};
+const unchecked = {
+  backgroundColor: '#fff',
+  border: '1px solid #000',
+  borderRadius: '3px',
+  display: 'inline-block',
+  width: '13px',
+  height: '13px',
+};
