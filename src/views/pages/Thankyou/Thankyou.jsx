cd67c3815fd84724c36826ab718919cbeeb0135d
umarzahir <umar.zahir@99technologies.co>
2024-04-05 13:46:58 +0500

    purchase data event data fixed

--- src/views/pages/Thankyou/Thankyou.jsx
+++ src/views/pages/Thankyou/Thankyou.jsx
@@ -13,6 +13,7 @@ import MobileThanku from './MobileThanku';
 import CircularProgress from '@mui/material/CircularProgress';
 import Box from '@mui/material/Box';
 import { getPaypalOrderDetail } from '../../../core/api/products';
+import { makeDataLayerItemObject } from '@utils/helpers';
 export default function ThankYou() {
   const [searchParams] = useSearchParams();
   const dispatch = useDispatch();
@@ -104,10 +105,11 @@ export default function ThankYou() {
       window.dataLayer.push({
         event: 'purchase',
         currency: 'USD',
-        // value: data.cartItem.price,
-        transaction_id: '123',
-        // items: makeDataLayerItemObject([{ ...data }]),
+        value: order?.total_amount,
+        transaction_id: order.id,
+        items: makeDataLayerItemObject(order?.order_item || []),
       });
+      debugger;
     } else {
       // navigate('/')
     }
