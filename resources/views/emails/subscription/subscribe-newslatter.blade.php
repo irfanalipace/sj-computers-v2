f007222a823d210b0b2fff8bbfcc6be6c3abb0fe
haris naseer satti <haris.naseer@99technologies.co>
2024-04-15 09:07:13 +0000

    Sc 774 subscription confirmation

--- resources/views/emails/subscription/subscribe-newslatter.blade.php
+++ resources/views/emails/subscription/subscribe-newslatter.blade.php
@@ -459,7 +459,7 @@
 <body>
 <!--*|IF:MC_PREVIEW_TEXT|*-->
 <!--[if !gte mso 9]><!----><span class="mcnPreviewText"
-                                 style="display:none; font-size:0px; line-height:0px; max-height:0px; max-width:0px; opacity:0; overflow:hidden; visibility:hidden; mso-hide:all;">*|MC_PREVIEW_TEXT|*</span>
+                                 style="display:none; font-size:0px; line-height:0px; max-height:0px; max-width:0px; opacity:0; overflow:hidden; visibility:hidden; mso-hide:all;"></span>
 <!--<![endif]-->
 <!--*|END:IF|*-->
 <center>
@@ -518,9 +518,10 @@
                                                                                                             <h1 style="margin: 0; font-size: 22px">
                                                                                                                 Subscription
                                                                                                                 Confirm!</h1>
-                                                                                                            <h2 style="font-size: 18px; padding: 10px 0px">
-                                                                                                                Hi
-                                                                                                                {{ $data->name ?? 'Customer' }}</h2>
+{{--                                                                                                            <h2 style="font-size: 18px; padding: 10px 0px">--}}
+{{--                                                                                                                Hi--}}
+{{--                                                                                                                {{ $data->name ?? 'Customer' }}</h2>--}}
+                                                                                                            <br>
                                                                                                             <p style="font-size: 14px">
                                                                                                                 Thank
                                                                                                                 you for
